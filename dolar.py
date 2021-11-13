3118f1d42550fcc0d9bb583d16cfe4efd325d5fb
Anna Absi <anna.absi@gmail.com>
2021-11-13 14:59:06 -0500

    cols fix

--- dolar.py
+++ dolar.py
@@ -6,25 +6,25 @@ import pandas as pd
 import requests
 
 
-
-def round_half_up(number,decimals):
-    if round((number * 10 ** (decimals + 1)) % 10 )== 5:
-        new_number=number+1/(10**(decimals+1))
+def round_half_up(number, decimals):
+    if round((number * 10 ** (decimals + 1)) % 10) == 5:
+        new_number = number+1/(10**(decimals+1))
         return round(new_number, decimals)
     else:
         return round(number, decimals)
 
 
 try:
-    
+
     url = f"https://estadisticas.bcrp.gob.pe/estadisticas/series/api/PD04639PD-PD04640PD-PD04637PD-PD04638PD/csv/2020-01-01/{datetime.today().strftime('%Y-%m-%d')}"
 
     print(url)
 
-    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
     req = requests.get(url, headers=headers)
-    req_text=str(req.text).replace("<br>","\n")
-    req_text=str(req_text).replace('"',"")
+    req_text = str(req.text).replace("<br>", "\n")
+    req_text = str(req_text).replace('"', "")
 
     dict_months = {
         "Ene": "01",
@@ -45,34 +45,33 @@ try:
     result = pattern.sub(lambda x: dict_months[x.group()], req_text)
     data = StringIO(result)
 
-    df=pd.read_csv(data, names=["FECHA","TC_SBS_COMPRA","TC_SBS_VENTA","TC_INTERBANCARIO_COMPRA","TC_INTERBANCARIO_VENTA"],skiprows=1)
-    df['FECHA']=pd.to_datetime(df['FECHA'], format='%d.%m.%y')
+    df = pd.read_csv(data, names=["FECHA", "TC_INTERBANCARIO_COMPRA", "TC_INTERBANCARIO_VENTA", "TC_SBS_COMPRA", "TC_SBS_VENTA"], skiprows=1)
+    df['FECHA'] = pd.to_datetime(df['FECHA'], format='%d.%m.%y')
 
+    df['TC_SBS_COMPRA'] = df['TC_SBS_COMPRA'].apply(round_half_up, decimals=3)
+    df['TC_SBS_VENTA'] = df['TC_SBS_VENTA'].apply(round_half_up, decimals=3)
 
-    df['TC_SBS_COMPRA']=df['TC_SBS_COMPRA'].apply(round_half_up,decimals=3)
-    df['TC_SBS_VENTA']=df['TC_SBS_VENTA'].apply(round_half_up,decimals=3)
+    df.to_csv("results/dolar.csv", index=False)
 
-    df.to_csv("results/dolar.csv",index=False)
+    df_sbs_com = df[["FECHA", "TC_SBS_COMPRA"]]
+    df_sbs_ven = df[["FECHA", "TC_SBS_VENTA"]]
+    df_int_com = df[["FECHA", "TC_INTERBANCARIO_COMPRA"]]
+    df_int_ven = df[["FECHA", "TC_INTERBANCARIO_VENTA"]]
 
-    df_sbs_com=df[["FECHA","TC_SBS_COMPRA"]]
-    df_sbs_ven=df[["FECHA","TC_SBS_VENTA"]]
-    df_int_com=df[["FECHA","TC_INTERBANCARIO_COMPRA"]]
-    df_int_ven=df[["FECHA","TC_INTERBANCARIO_VENTA"]]
+    df_sbs_com.columns = ['time', 'value']
+    df_sbs_ven.columns = ['time', 'value']
+    df_int_com.columns = ['time', 'value']
+    df_int_ven.columns = ['time', 'value']
 
-    df_sbs_com.columns=['time','value']
-    df_sbs_ven.columns=['time','value']
-    df_int_com.columns=['time','value']
-    df_int_ven.columns=['time','value']
+    df_sbs_com['time'] = df_sbs_com['time'].dt.strftime('%Y-%m-%d')
+    df_sbs_ven['time'] = df_sbs_ven['time'].dt.strftime('%Y-%m-%d')
+    df_int_com['time'] = df_int_com['time'].dt.strftime('%Y-%m-%d')
+    df_int_ven['time'] = df_int_ven['time'].dt.strftime('%Y-%m-%d')
 
-    df_sbs_com['time']=df_sbs_com['time'].dt.strftime('%Y-%m-%d')
-    df_sbs_ven['time']=df_sbs_ven['time'].dt.strftime('%Y-%m-%d')
-    df_int_com['time']=df_int_com['time'].dt.strftime('%Y-%m-%d')
-    df_int_ven['time']=df_int_ven['time'].dt.strftime('%Y-%m-%d')
-
-    df_sbs_com.to_json("results/timeseries_by_tc/TC_SBS_COMPRA.json",orient="records")
-    df_sbs_ven.to_json("results/timeseries_by_tc/TC_SBS_VENTA.json",orient="records")
-    df_int_com.to_json("results/timeseries_by_tc/TC_INTERBANCARIO_COMPRA.json",orient="records")
-    df_int_ven.to_json("results/timeseries_by_tc/TC_INTERBANCARIO_VENTA.json",orient="records")
+    df_sbs_com.to_json("results/timeseries_by_tc/TC_SBS_COMPRA.json", orient="records")
+    df_sbs_ven.to_json("results/timeseries_by_tc/TC_SBS_VENTA.json", orient="records")
+    df_int_com.to_json("results/timeseries_by_tc/TC_INTERBANCARIO_COMPRA.json", orient="records")
+    df_int_ven.to_json("results/timeseries_by_tc/TC_INTERBANCARIO_VENTA.json", orient="records")
 
 except ConnectionResetError:
     pass
